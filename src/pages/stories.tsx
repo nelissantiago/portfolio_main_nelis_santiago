ccf6772dd3aaa4f9be5618980e34020252ceeaff
unknown <slaidezera.web2021@gmail.com>
2022-09-26 02:09:17 -0300

    first commit

--- src/pages/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Stories } from "../components/dashboard/Home/stories";
-
-export default function FuncioesStories() {
-    return (
-        <>
-            <Stories />
-        </>
-    )
-}
